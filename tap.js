3e055394e7d7a84671ba16255150722e5c4c68ae
AzureAD\박인태 <pit1995@outlook.kr>
2024-12-16 15:36:01 +0900

    데이터 바인딩

--- tap.js
+++ tap.js
@@ -1,10 +1,17 @@
+// product detail
+let car = {name : '소나타', price :[50000,30000,40000]}
+$('.item').text(car.name);
+$('.price').text(car.price[0]);
+// listButton
 const button = $('.tab-button');
 const contents = $('.tab-content');
 button.on('click', function(){
     const index = button.index($(this));
-
+    openTab()
+})
+function openTab(){
     button.removeClass('orange');
     button.eq(index).addClass('orange');
     contents.removeClass('show');
     contents.eq(index).addClass('show');
-})
+}
