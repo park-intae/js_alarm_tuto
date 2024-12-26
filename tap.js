1f18825e9d20e2d93a7b084eee6f8d33c5584f56
AzureAD\박인태 <pit1995@outlook.kr>
2024-12-26 23:55:26 +0900

    과제, 상품 종류 선택 시 수량 변경 모시깽

--- tap.js
+++ tap.js
@@ -1,17 +1,27 @@
 // product detail
-let car = {name : '소나타', price :[50000,30000,40000]}
+let car = { name: '소나타', price: [50000, 30000, 40000] }
 $('.item').text(car.name);
 $('.price').text(car.price[0]);
 // listButton
 const button = $('.tab-button');
 const contents = $('.tab-content');
-button.on('click', function(){
+button.on('click', function () {
     const index = button.index($(this));
-    openTab()
+    openTab(index)
 })
-function openTab(){
+function openTab(index) {
     button.removeClass('orange');
     button.eq(index).addClass('orange');
     contents.removeClass('show');
     contents.eq(index).addClass('show');
 }
+//셔츠 선택지 함수
+$('.custom-select').eq(0).on('input', function () {
+    let value = $(this).val();
+    if (value == '셔츠') {
+        $('.custom-select').eq(1).removeClass('form-hide');
+    } else {
+        $('.custom-select').eq(1).addClass('form-hide');
+
+    }
+})
